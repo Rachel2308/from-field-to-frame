c3be64680bcd596439c511415ddd421ca922964b
Rachel2308 <rachelsherlock@yahoo.co.uk>
2021-07-02 10:32:11 +0000

    Furniture app added

---
+++ furniture/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Furniture(models.Model):
+    title = models.CharField(max_length=250, null=True, blank=True)
+    product = models.CharField(max_length=250)
+    medium = models.CharField(max_length=100, null=True, blank=True)
+    description = models.TextField()
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
