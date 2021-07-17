a46fa44759da8dbee7e7bdc3cb901a73bd24d6c4
Rachel2308 <rachelsherlock@yahoo.co.uk>
2021-07-17 16:10:18 +0000

    updated contexts.py for add_to_basket

--- basket/contexts.py
+++ basket/contexts.py
@@ -1,10 +1,23 @@
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from art.models import Art
 
 def basket_contents(request):
 
     basket_items = []
     total = 0
-    product_count = 0
+    art_count = 0
+    basket = request.session.get('basket', {})
+
+    for item_id, quantity in basket.items():
+        art = get_object_or_404(Art, pk=item_id)
+        total += quantity * art.price
+        art_count += quantity
+        basket_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'art': art,
+        })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = 4.99
@@ -19,7 +32,7 @@ def basket_contents(request):
     context = {
         'basket_items': basket_items,
         'total': total,
-        'product_count': product_count,
+        'art_count': art_count,
         'delivery': delivery,
         'free_delivery_delta': free_delivery_delta,
         'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
