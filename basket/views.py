aaf63e0147249253daf4abd9128259107d667921
Rachel2308 <rachelsherlock@yahoo.co.uk>
2021-07-20 20:41:28 +0000

    Toasts info, error and warning added to site. Toast_success amended

--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 
 from art.models import Art
@@ -21,6 +21,8 @@ def add_to_basket(request, item_id):
 
     if item_id in list(basket.keys()):
         basket[item_id] += quantity
+        messages.success(request, f' Updated quantity of {art.title} to {basket[item_id]}')
+
     else:
         basket[item_id] = quantity
         messages.success(request, f'{art.title} added to your basket')
@@ -32,13 +34,18 @@ def add_to_basket(request, item_id):
 def adjust_basket(request, item_id):
     """A view to adjust the quantity of products in the basket"""
 
+    art = get_object_or_404(Art, pk=item_id)
     basket = request.session.get('basket', {})
     quantity = int(request.POST.get('quantity'))
 
     if quantity > 0:
         basket[item_id] = quantity
+        messages.success(request, f' Updated quantity of {art.title} to {basket[item_id]}')
+
     else:
         basket.pop(item.id)
+        messages.success(request, f'{art.title} removed from your basket')
+
 
     request.session['basket'] = basket
     return redirect(reverse('view_basket'))
@@ -48,12 +55,16 @@ def remove_from_basket(request, item_id):
     """A view to remove products from the shopping basket"""
 
     try:
+        art = get_object_or_404(Art, pk=item_id)
         basket = request.session.get('basket', {})
 
         basket.pop(item_id)
 
+        messages.success(request, f'{art.title} removed from your basket')
         request.session['basket'] = basket
         return HttpResponse(status=200)
 
+
     except Exception as e:
+        messages.error(request, f'Error removing {art.title}: {e}')
         return HttpResponse(status=500)
