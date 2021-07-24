c43d282b940620eb032925b764fd95a016e00f54
Rachel2308 <rachelsherlock@yahoo.co.uk>
2021-07-24 17:13:55 +0000

    Profiles app, models, URL and views added

--- from_field_to_frame/settings.py
+++ from_field_to_frame/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'blog',
     'basket',
     'checkout',
+    'profiles',
 
     # Other
 
--- from_field_to_frame/urls.py
+++ from_field_to_frame/urls.py
@@ -27,5 +27,6 @@ urlpatterns = [
     path('blog/', include('blog.urls')),
     path('basket/', include('basket.urls')),
     path('checkout/', include('checkout.urls')),
+    path('profile/', include('profiles.urls')),
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
