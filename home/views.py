b3d9d94d44015d5932a51fb213a53ca1d269b2a7
Rachel2308 <rachelsherlock@yahoo.co.uk>
2021-07-30 16:34:00 +0000

    contact form views.py added

--- home/views.py
+++ home/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Create your views here.
 
@@ -10,5 +10,24 @@ def index(request):
 
 def contact(request):
     """View to return the contact page"""
-    
-    return render(request, 'home/contact.html')
+
+    if request.method =='POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            subject = "Website Enquiry"
+            body = {
+                'first_name': form.cleaned.data['first_name'],
+                'last_name': form.cleaned.data['last_name'],
+                'email': form.cleaned.data['email_address'],
+                'message': form.cleaned.data['message'],
+            }
+            message = "\n".join(body.values())
+
+            try:
+                send_mail(subject, message, 'fromfieldtoframe21@gmail.com', ['fromfieldtoframe21@gmail.com'])
+            except BadHeaderError:
+                return HttpResponse('Invalid Header Found')
+            return redirect ('home')
+
+    form = ContactForm()
+    return render(request, 'home/contact.html', {'form':form})
